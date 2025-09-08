2067e9b031a2b561dc83820ef2d652a2d95e8f9b
HimikoEgal <alison.chavez.jimenez99@gmail.com>
2025-09-08 00:41:27 -0600

    Tabla_mult

--- Tabla de multiplicar/Tabla_mult.C
+++ Tabla de multiplicar/Tabla_mult.C
@@ -2,5 +2,26 @@
 Hacer un programa que muestre una tabla de multiplicar hasta el 20 de un numero cualquiera por pantalla,
 el numero se pedira en el programa principal. Usar funcion.
 
-n * 1, n * 2, ... n * 20 while/for/dentro printf con resultados
+n * 1, n * 2, ... n * 20 void for/dentro printf con resultados
 */
+
+#include <stdio.h>
+
+void tabla_mult(int n);
+
+int main() {
+    
+    int n, i, count;
+    printf("Ingrese un numero para multiplicar hasta el 20: ");scanf("%i", &n);
+    tabla_mult(n);
+
+    return 0;
+}
+
+void tabla_mult(int n) {
+    int i, count;
+    for(i=1; i<=20; i++) {
+        count = n * i;
+        printf("%i * %i = %i\n", n, i, count);
+    }
+}
